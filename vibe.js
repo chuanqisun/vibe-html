d05fe58b79dee534b8eab0e890d535bb48d21f01
chuanqisun <chuanqi.sun.dev@gmail.com>
2025-03-14 21:43:20 -0700

    test syntax

--- vibe.js
+++ vibe.js
@@ -1,9 +1,30 @@
 document.addEventListener("click", (e) => {
   if (!e.ctrlKey) {
-    document.querySelectorAll("[selected]").forEach((el) => {
+    document.querySelectorAll("[data-selected]").forEach((el) => {
       if (el === e.target) return;
-      el.removeAttribute("selected");
+      el.removeAttribute("data-selected");
     });
   }
-  e.target.toggleAttribute("selected");
+  e.target.toggleAttribute("data-selected");
 });
+
+customElements.define(
+  "v-icon",
+  class extends HTMLElement {
+    static get observedAttributes() {
+      return ["href", "size", "width", "height"];
+    }
+
+    connectedCallback() {
+      const width = this.getAttribute("size") ?? this.getAttribute("width") ?? 16;
+      const height = this.getAttribute("height") ?? this.getAttribute("size") ?? 16;
+      this.innerHTML = `<svg width="${width}" height="${height}"><use xlink:href="${this.getAttribute("href")}" /></svg>`;
+    }
+
+    attributeChangedCallback() {
+      const width = this.getAttribute("size") ?? this.getAttribute("width") ?? 16;
+      const height = this.getAttribute("height") ?? this.getAttribute("size") ?? 16;
+      this.innerHTML = `<svg width="${width}" height="${height}"><use xlink:href="${this.getAttribute("href")}" /></svg>`;
+    }
+  }
+);
